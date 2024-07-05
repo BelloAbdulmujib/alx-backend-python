c346019ded15e67c406a4d265b042d827b7c97d4
BelloAbdulmujib <belloabdulmujib3@gmil.com>
2024-07-04 13:19:38 -1100

    0x00-python_variable_annotations

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -7,10 +7,3 @@ def to_str(n: float) -> str:
     '''Cast a floting type number in to a string.
     '''
     return str(n)
-
-
-if __name__ == '__main__':
-    print(f'to_str(3.142)')
-    print(to_str(0.00))
-
-    print(to_str.__annotations__)
