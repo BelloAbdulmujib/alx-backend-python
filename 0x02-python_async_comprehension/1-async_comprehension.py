6571c06b2b83cead2d1bc5eb2bffc8a5f2015e55
BelloAbdulmujib <belloabdulmujib3@gmil.com>
2024-07-09 12:06:54 -1100

    0x02-python_async_comprehension

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,11 +1,11 @@
-#!/usr/bin/python3
-'''Task 1's module
+#!/usr/bin/env python3
+'''Task 1's module.
 '''
 from typing import List
 from importlib import import_module as using
 
 
-async_generator = using('0-asyn_generator').async_generator
+async_generator = using('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
